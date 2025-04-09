9f1981be5579514239f6864673e9f4172840fb4e
sujo030902 <dhksujal03@gmail.com>
2025-04-09 21:34:18 +0530

    added clicking functionality

--- src/assets/components/Board.jsx
+++ src/assets/components/Board.jsx
@@ -1,16 +1,22 @@
 import { useState } from 'react';
 
-function Square({ value }) {
-  return <button className="square">{value}</button>;
+function Square({ value, onSquareClick }) {
+  return <button className="square" onClick={onSquareClick}>{value}</button>;
 }
 
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(){
+  const nextSquares = squares.slice();
+  nextSquares[i] = "X";
+  setSquares(nextSquares);
+  }
   
   return (
     <>
       <div className="board-row">
-        <Square value={squares[0]} />
+        <Square value={squares[0]} onSquareClick = {handleClick} />
         <Square value={squares[1]} />
         <Square value={squares[2]} />
       </div>
