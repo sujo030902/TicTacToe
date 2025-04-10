b0a0d6280436b9a70c1a662c3f2a38f6fde86ba6
sujo030902 <dhksujal03@gmail.com>
2025-04-10 15:46:01 +0530

    using custom hooks

--- src/assets/components/TicTacToe.jsx
+++ src/assets/components/TicTacToe.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 
-const initialValue = () => Array(9).fill(null);
+
 
 const TicTacToe = () => {
-  const [board, setboard] = useState(initialValue());
+
 
   return (
     <>
---
+++ src/hooks/useTicTacToe.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+const initialValue = () => Array(9).fill(null);
+
+const useTicTacToe = ()=>{
+    const [board, setboard] = useState(initialValue());
+    const [isNext, setisNext] = useState(true);
+
+    const winning_Patterns = [];
+    
+    const calculateWinner = () => {}
+
+    const handleClick = () => {}
+
+    const getStatusMessage = () => {}
+
+    const resetGame = () => {}
+
+    return {board, calculateWinner, handleClick, getStatusMessage, resetGame}
+}
