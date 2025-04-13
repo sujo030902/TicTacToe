f74fbde6631777d6ff54dd0fd7380fab30cb0e1b
sujo030902 <dhksujal03@gmail.com>
2025-04-13 22:45:18 +0530

    defined winning patterns

--- src/hooks/useTicTacToe.jsx
+++ src/hooks/useTicTacToe.jsx
@@ -6,11 +6,13 @@ const useTicTacToe = ()=>{
     const [board, setboard] = useState(initialValue());
     const [isNext, setisNext] = useState(true);
 
-    const winning_Patterns = [];
+    const winning_Patterns = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
     
     const calculateWinner = () => {}
 
-    const handleClick = (index) => {}
+    const handleClick = (index) => {
+        
+    }
 
     const getStatusMessage = () => {}
 
