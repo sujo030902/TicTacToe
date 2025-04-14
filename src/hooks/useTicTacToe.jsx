c7633ee00d824b680f98802e3a5990288a3812b0
sujo030902 <dhksujal03@gmail.com>
2025-04-14 21:10:00 +0530

    defined calculation of winner logic

--- src/hooks/useTicTacToe.jsx
+++ src/hooks/useTicTacToe.jsx
@@ -8,7 +8,15 @@ const useTicTacToe = ()=>{
 
     const winning_Patterns = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
     
-    const calculateWinner = () => {}
+    const calculateWinner = () => {
+        for(let patterns of winning_Patterns){
+            const [a,b,c] = patterns;
+            if (board[a] && board[a] == board[b] && board[a]==board[c]){
+                return board[a];
+            }
+        }
+        return null;
+    }
 
     const handleClick = (index) => {
         
